bf584ca8f093913b9dd9540cab64e7e075476932
Shaik-Mohammed-Zubaidi <68868510+Shaik-Mohammed-Zubaidi@users.noreply.github.com>
2020-11-08 05:10:19 +0530

    dfbdfb

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,16 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component, useState } from "react";
+import "../styles/App.css";
 
 const App = () => {
   return (
-    <></>
-  )
-}
-
+    <div data-ns-test>
+      <h1 data-ns-test="project-name">What To Do!</h1>
+      <p data-ns-test="project-description">
+        A site which will show you a list of things you can do from games to
+        courses and also keeps tracks of your progresses
+      </p>
+    </div>
+  );
+};
 
 export default App;
